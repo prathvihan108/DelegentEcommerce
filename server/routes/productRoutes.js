fa5f3edc4941d545f18d21584aa9fbbcd730b787
Prathvihan1008 <prathvihan1008@gmail.com>
2025-11-10 13:39:39 +0530

    import fixed for the auth route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getProducts, addProduct } from "../controllers/productController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
